869466703a252296799b9f9b2f85c1953ad50182
Algis Dumbris <a.dumbris@gmail.com>
2018-05-03 19:02:02 +0300

    Working on data transfer objects and protocol implementation

--- intlabeler/const.py
+++ intlabeler/const.py
@@ -8,3 +8,13 @@ MSG_TYPE_TASK = 'task'
 MSG_TYPE_SOLUTION = 'solution'
 MSG_TYPE_UPDATE = 'update'
 MSG_TYPE_STOP = 'stop'
+#Data fields
+MSG_DATA_TITLE = 'title'
+MSG_DATA_X = 'x'
+MSG_DATA_Y = 'y'
+MSG_DATA_LABEL_NAME = 'label_name'
+MSG_DATA_LABEL_TYPE = 'label_type'
+#Task types
+LABEL_TYPE_MULTICLASS = 'multiclass'
+LABEL_TYPE_MULTILABEL = 'multilabel'
+LABEL_TYPE_BINARY = 'binary'
--- intlabeler/protocol.py
+++ intlabeler/protocol.py
@@ -1,23 +1,23 @@
-import const
+import intlabeler.const as const
+from intlabeler.dto_base import AsDict
 from collections import namedtuple
 import json
 
 MSG_VERSION_VAL = '1.0'
 
-class Base(object):
-    def __init__(self, type_, version):
-        self.type = type_
-        self.version = version
+class Version():
+    version = MSG_VERSION_VAL
 
-    def to_json(self):
-        return json.dumps(self.__dict__)
-
-    @classmethod
-    def from_json(cls, json_str):
-        json_dict = json.loads(json_str)
-        return cls(**json_dict)
+class Data(namedtuple("Data", [ const.MSG_DATA_X, 
+                                const.MSG_DATA_LABEL_NAME, 
+                                const.MSG_DATA_TITLE, 
+                                const.MSG_DATA_LABEL_TYPE,
+                                const.MSG_DATA_Y, 
+                              ]), AsDict):
+    pass
 
 
-class ServerTask(Base):
-    def __init__(self, *args, **kwargs):
-        super().__init__(const.MSG_TYPE_TASK, MSG_VERSION_VAL)
+
+class ServerMsg(namedtuple("ServerMsg", [const.MSG_DATA, const.MSG_TYPE, const.MSG_ID]), AsDict, Version):
+    pass
+
